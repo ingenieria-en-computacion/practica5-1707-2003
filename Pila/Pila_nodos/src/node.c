a41e77cefefb9caede041640ab443c2497f830b1
Cantero Martel Mariana Itzel <cantero.martel@gmail.com>
2025-03-20 19:30:01 -0600

    ejercicio 11

--- Pila/Pila_nodos/src/node.c
+++ Pila/Pila_nodos/src/node.c
@@ -1,4 +1,6 @@
 #include "node.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * Crea un nuevo nodo con los datos proporcionados y lo devuelve.
@@ -11,7 +13,17 @@
  *          proporcionado en el parámetro `d`, el siguiente .
  */
 Node *new_node(Data d){
+    // Asignar memoria dinámicamente para un nuevo nodo
+    Node* new_n = (Node*) malloc(sizeof(Node));
 
+    if (new_n == NULL) {
+        return NULL;  // Si no se pudo asignar memoria, devolver NULL
+    }
+
+    new_n->data = d;  // Establecer el dato proporcionado en el nodo
+    new_n->next = NULL;  // El siguiente nodo es NULL, ya que es el último nodo
+
+    return new_n;
 }
 
 /**
@@ -24,7 +36,9 @@ Node *new_node(Data d){
  *          de ser eliminado. Está función solo libera nodos cuyo enlace al siguiente es nulo
  */
 void delete_node(Node* n){
-
+    if (n != NULL) {
+        free(n);  // Liberar la memoria del nodo
+    }
 }
 
 /**
@@ -36,5 +50,10 @@ void delete_node(Node* n){
  *          indicando que el nodo es inválido. La salida se dirige a la salida estándar (stdout).
  */
 void print_node(Node* n){
-
+    if (n == NULL) {
+        printf("Nodo inválido.\n");
+    } else {
+        printf("Dato: %d\n", n->data);  // Imprimir el dato almacenado en el nodo
+        printf("Siguiente: %p\n", (void*)n->next);  // Imprimir la dirección del siguiente nodo (NULL si es el último)
+    }
 }
