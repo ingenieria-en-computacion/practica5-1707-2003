684a147576d97eb1b5cafa7620072e3d0a404170
Cantero Martel Mariana Itzel <cantero.martel@gmail.com>
2025-03-20 19:03:01 -0600

    ejercicio2

--- Cola/Cola_arreglos/src/queue.h
+++ Cola/Cola_arreglos/src/queue.h
@@ -2,18 +2,23 @@
 #define __QUEUE_H__
 
 #include <stdbool.h>
-typedef int Data;
-#define TAM 100
 
+// Definimos el tipo de dato que usaremos para almacenar los elementos en la cola
+typedef int Data;
+typedef int data;
+#define TAM 100  // Tamaño máximo de la cola
+
+// Definimos la estructura de la cola
 typedef struct {
-    Data datos[TAM];
-    int head;
-    int tail;
-    int len
+    Data datos[TAM];  // Arreglo de datos para almacenar los elementos de la cola
+    int head;          // Índice para el primer elemento de la cola
+    int tail;          // Índice para el siguiente espacio disponible en la cola
+    int len;           // Longitud actual de la cola
 } Queue;
 
+// Funciones que se implementarán para trabajar con la cola
 Queue queue_create();
-void queue_enqueue(Queue* , Data);
+void queue_enqueue(Queue*, Data);
 Data queue_dequeue(Queue*);
 bool queue_is_empty(Queue*);
 Data queue_front(Queue*);
