4a737d5d402e661e64117c03f02d809c4bf45b59
Cantero Martel Mariana Itzel <cantero.martel@gmail.com>
2025-03-20 19:25:45 -0600

    ejercicio8

--- Pila/Pila_arreglos/src/stack.h
+++ Pila/Pila_arreglos/src/stack.h
@@ -2,18 +2,23 @@
 #define __STACK_H__
 
 #include <stdbool.h>
-typedef int Data;
-#define TAM 100
 
+// Definimos el tamaño máximo de la pila
+#define MAX_SIZE 100  // Cambié 'TAM' por 'MAX_SIZE' como convención
+
+typedef int Data;  // Definimos que 'Data' es de tipo int, ajusta según necesites
+
+// Definimos la estructura de la pila
 typedef struct {
-    Data data[TAM];
-    int top;
+    Data data[MAX_SIZE];  // Arreglo que contiene los elementos de la pila
+    int top;  // Índice que apunta al elemento superior de la pila
 } Stack;
 
+// Declaración de las funciones
 Stack stack_create();
 void stack_push(Stack*, Data);
 Data stack_pop(Stack*);
-bool stack_is_empty(Stack* );
+bool stack_is_empty(Stack*);
 void stack_empty(Stack*);
 void stack_print(Stack *);
 
