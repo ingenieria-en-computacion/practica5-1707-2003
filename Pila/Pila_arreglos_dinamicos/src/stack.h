03b5b964b1dbb158c03aaf34c0d3cd8b5d96fd24
Cantero Martel Mariana Itzel <cantero.martel@gmail.com>
2025-03-20 19:28:22 -0600

    ejercicio10

--- Pila/Pila_arreglos_dinamicos/src/stack.h
+++ Pila/Pila_arreglos_dinamicos/src/stack.h
@@ -2,6 +2,9 @@
 #define __STACK_H__
 #include <stdbool.h>
 
+// Definimos el tamaño máximo de la pila
+#define MAX_SIZE 100  // Puedes cambiar este valor si necesitas un tamaño diferente
+
 typedef int Data;
 
 typedef struct {
@@ -12,7 +15,7 @@ typedef struct {
 Stack stack_create(int);
 void stack_push(Stack*, Data);
 Data stack_pop(Stack*);
-bool stack_is_empty(Stack* );
+bool stack_is_empty(Stack*);
 void stack_delete(Stack *);
 void stack_empty(Stack*);
 void stack_print(Stack *);
